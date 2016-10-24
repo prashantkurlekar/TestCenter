371ca2f0ecd30deedcdc5badb4fd6476a3466393
Prashant Kurlekar <prashantkurlekar@gmail.com>
2016-10-24 19:17:19 +1100

    added unit test

--- app/TestCenter.Ionic/src/pages/about/about.spec.ts
+++ app/TestCenter.Ionic/src/pages/about/about.spec.ts
@@ -1,16 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
-import { HomePage } from './home';
+import { AboutPage } from './about';
 import { async, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { mockNavController, mockPlatform } from 'ionic-angular/util/mock-providers';
 import { NavController, Platform } from 'ionic-angular';
 
-describe('Pages: HomePage', () => {
+describe('Pages: AboutPage', () => {
 
   beforeEach((done) => {
     TestBed.configureTestingModule({
-      declarations: [HomePage],
+      declarations: [AboutPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         { provide: NavController, useValue: mockNavController },
@@ -21,7 +21,7 @@ describe('Pages: HomePage', () => {
   });
 
   it('should load component', async(() => {
-    const fixture: any = TestBed.createComponent(HomePage);
+    const fixture: any = TestBed.createComponent(AboutPage);
     fixture.detectChanges();
     let component: any = fixture.debugElement.componentInstance;
 
--- app/TestCenter.Ionic/src/pages/contact/contact.spec.ts
+++ app/TestCenter.Ionic/src/pages/contact/contact.spec.ts
@@ -1,16 +1,16 @@
 /* tslint:disable:no-unused-variable */
 
-import { HomePage } from './home';
+import { ContactPage } from './contact';
 import { async, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { mockNavController, mockPlatform } from 'ionic-angular/util/mock-providers';
 import { NavController, Platform } from 'ionic-angular';
 
-describe('Pages: HomePage', () => {
+describe('Pages: ContactPage', () => {
 
   beforeEach((done) => {
     TestBed.configureTestingModule({
-      declarations: [HomePage],
+      declarations: [ContactPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
         { provide: NavController, useValue: mockNavController },
@@ -21,7 +21,7 @@ describe('Pages: HomePage', () => {
   });
 
   it('should load component', async(() => {
-    const fixture: any = TestBed.createComponent(HomePage);
+    const fixture: any = TestBed.createComponent(ContactPage);
     fixture.detectChanges();
     let component: any = fixture.debugElement.componentInstance;
 
